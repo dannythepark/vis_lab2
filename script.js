45d8622f08daa9ce6ecb0cec0bb414428b1c6d81
Daniel Park <parkant@bc.edu>
2021-09-18 02:48:39 -0400

    Lab2

--- script.js
+++ script.js
@@ -6,29 +6,35 @@ fetch('attractions.json')
   .then(response => response.json())
   .then(data => {
 		attractions = data;
-		filterData(attractions);
-		//console.log('attractions - (a)', attractions);
-		
-
-
+		console.log(attractions)
+		filterData();		
 
 function filterData(category) {
-	console.log(category)
-	category.sort((a, b) => (a.Visitors < b.Visitors ? 1 : -1));
-	attractions = category.slice(0,5);
-	console.log(attractions)
-	renderBarChart(attractions)
+	let filtered = attractions; 
+	console.log(filtered)
+	if (category && category != "all"){
+		 filtered = filtered.filter(function(row,index){
+			 return row.Category == category;
+		 });
+	}
+	const descending_sorted = filtered.sort(function(a,b){
+		return b.Visitors - a.Visitors;
+	});
+
+	let top_five = descending_sorted.slice(0,5);
+
+	renderBarChart(top_five)
 
 
-
-
+	///attractions.sort((a, b) => (a.Visitors < b.Visitors ? 1 : -1));
+	//attractions = category.slice(0,5);
+	//console.log("top 5 attractions by Visitors : ");
+	//console.log(attractions)
 }
-
 // TODO: Define an event listener for the dropdown menu
 //       Call filterData with the selected category
 
-document.querySelector("#attraction-category").addEventListener("change", function (attractions){
-	filterData(attractions.target.value);
+document.querySelector("#attraction-category").addEventListener("change", function (event){
+	filterData(event.target.value);
 })
-
   });
